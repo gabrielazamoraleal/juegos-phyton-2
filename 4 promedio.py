a5a6c4c8271d78e3da5800f03883ae4bd0febf24
gabrielazamoraleal <gabriela.zamora63@gmail.com>
2021-03-11 18:58:45 -0600

    nuevo
    nuevo

---
+++ 4 promedio.py
@@ -0,0 +1,31 @@
+"""
+4 promedio
+En una universidad cada estudiante cursa 4 materias en el semestre. 
+Desarrolla un programa que pregunte la calificación de cada materia, calcula el promedio de las 4 materias y muestra el resultado.
+El programa debe llamarse Promedio.py
+
+entrada
+1
+2
+3
+4
+salida
+2.5
+entrada
+10
+20
+20
+24
+salida
+18.5
+"""
+
+
+# Autor:Fabiola Cruz González
+c1=float(input('numero 1 '))
+c2=float(input('numero 2 '))
+c3=float(input('numero 3 '))
+c4=float(input('numero 4 '))
+promedio = (c1+c2+c3+c4)/4
+print ('el promedio es ')
+print (promedio, ' $ ')
